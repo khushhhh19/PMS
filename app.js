b6dca16562b7f95e913c64e30ba3d29c36f2535e
Khushboo Agarwal <khushbooa.1999@gmail.com>
2022-10-04 16:10:16 +0530

    changes 2

---
+++ app.js
@@ -0,0 +1,15 @@
+document.getElementById("button").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "flex";
+});
+
+document.getElementById("button2").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "flex";
+});
+
+document.getElementById("button3").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "none";
+});
